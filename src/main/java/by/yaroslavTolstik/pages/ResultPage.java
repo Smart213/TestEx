20794241553897fe64c176dd17d228b9aea31746
Толстик Ярослав Вячеславович <yaraslau.toustsik@softclub.by>
2021-08-19 06:54:14 +0300

    update assertions

--- src/main/java/by/yaroslavTolstik/pages/ResultPage.java
+++ src/main/java/by/yaroslavTolstik/pages/ResultPage.java
@@ -1,17 +1,15 @@
 package by.yaroslavTolstik.pages;
 
+import by.yaroslavTolstik.entity.ResultTo;
 import by.yaroslavTolstik.entity.SearchTo;
-import by.yaroslavTolstik.utils.ParseUtil;
+import by.yaroslavTolstik.entity.factorys.ResultToFactory;
 import lombok.extern.log4j.Log4j;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static by.yaroslavTolstik.BaseElements.*;
-import static by.yaroslavTolstik.utils.DateUtil.getDateToResult;
 import static java.util.stream.Collectors.toList;
 
 @Log4j
@@ -20,101 +18,41 @@ public class ResultPage {
     private final String results = "div[data-testid$='ticket-desktop'] ";
     private final String timer = "div[class$='search-countdown__timer']";
 
-    private final String resultsForth = "div[data-testid$='forth'] ";
-    private final String resultsBack = "div[data-testid$='back'] ";
+    public final static String resultsForth = "div[data-testid$='forth'] ";
+    public final static String resultsBack = "div[data-testid$='back'] ";
 
-    private final String destinationEndpoint = "div[data-testid='destination-endpoint'] ";
-    private final String originEndpoint = "div[data-testid='origin-endpoint'] ";
+    public final static String destinationEndpoint = "div[data-testid='destination-endpoint'] ";
+    public final static String originEndpoint = "div[data-testid='origin-endpoint'] ";
 
-    private final String city = "div[data-testid$='city']";
-    private final String date = "div[data-testid$='date']";
+    public final static String city = "div[data-testid$='city']";
+    public final static String date = "div[data-testid$='date']";
 
-    private final String amount = "span[class='buy-button__price'] span[data-test-element$='price']";
+    public final static String amount = "span[class='buy-button__price'] span[data-test-element$='price']";
 
-    /**
-     * проверка городов вылета туда
-     */
-    public ResultPage assertCitiesFromForth(SearchTo to) {
-        $$(results + resultsForth + originEndpoint + city).forEach(el ->
-                Assert.assertEquals(el.getText(), to.getFrom().getName(),
-                        "Город вылета не соответствует " + to.getFrom().getName()));
-        return this;
-    }
-
-    /**
-     * проверка городов приземления туда
-     */
-    public ResultPage assertCitiesToForth(SearchTo to) {
-        $$(results + resultsForth + destinationEndpoint + city).forEach(el ->
-                Assert.assertEquals(el.getText(), to.getTo().getName(),
-                        "Город прилета не соответствует " + to.getTo().getName()));
-        return this;
-    }
-
-    /**
-     * проверка городов вылета назад
-     */
-    public ResultPage assertCitiesFromBack(SearchTo to) {
-        $$(results + resultsBack + originEndpoint + city).forEach(el ->
-                Assert.assertEquals(el.getText(), to.getTo().getName(),
-                        "Город вылета обратного рейса не соответствует " + to.getTo().getName()));
-        return this;
-    }
-
-    /**
-     * проверка городов приземления назад
-     */
-    public ResultPage assertCitiesToBack(SearchTo to) {
-        $$(results + resultsBack + destinationEndpoint + city).forEach(el ->
-                Assert.assertEquals(el.getText(), to.getFrom().getName(),
-                        "Город прилета обратного рейса не соответствует " + to.getFrom().getName()));
-        return this;
-    }
-
-    /**
-     * проверка дат вылета туда
-     */
-    public ResultPage assertDateFromForth(SearchTo to) {
-        $$(results + resultsForth + originEndpoint + date).forEach(el ->
-                Assert.assertEquals(el.getText(), getDateToResult(to.getDateFrom()),
-                        "Дата вылета не соответствует " + to.getFrom().getName()));
+    public ResultPage assertFields(SearchTo to) {
+        ResultTo exp = ResultToFactory.create(to);
+        getResults().forEach(act ->
+                Assert.assertEquals(act, exp, "Данные отображены не верно."));
         return this;
     }
 
 
-    /**
-     * проверка всех дат вылета назад
-     */
-    public ResultPage assertDateFromBack(SearchTo to) {
-        $$(results + resultsBack + originEndpoint + date).forEach(el ->
-                Assert.assertEquals(el.getText(), getDateToResult(to.getDateBack()),
-                        "Дата вылета обратного рейса не соответствует " + to.getTo().getName()));
-        return this;
+    private List<ResultTo> getResults() {
+        return $$(results)
+                .stream()
+                .map(ResultToFactory::create)
+                .collect(toList());
     }
-
 
     /**
      * проверяет отсортирован ли по цене результат поиска
      */
     public ResultPage assertAmountSorted() {
-        final List<Long> actlist =
-                $$(results + amount)
-                        .stream()
-                        .map(WebElement::getText)
-                        .map(ParseUtil::getLong)
-                        .collect(toList());
-        List<Long> sortedList =
+        final List<ResultTo> actlist = getResults();
+        List<ResultTo> sortedList =
                 actlist.stream()
                         .sorted()
                         .collect(Collectors.toList());
-        Assert.assertEquals(
-                ParseUtil.getLong($$(results + amount).get(0).getText()),
-                Collections.min(actlist),
-                "первое значение не минимальное.");
-        Assert.assertEquals(
-                ParseUtil.getLong($$(results + amount).get(actlist.size() - 1).getText()),
-                Collections.max(actlist),
-                "последнее значение не максимальное.");
         Assert.assertEquals(actlist, sortedList, "Список цен не отсортирован");
         return this;
     }
